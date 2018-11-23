9f000c85ae96d37bbb957d93dfaef40bf7be7ece
wlt30 <wlt@ualberta.ca>
2018-11-23 12:55:50 -0700

    finished melding phase 2 with main function

--- Sandbox/phase2.py
+++ Sandbox/phase2.py
@@ -1,34 +1,83 @@
 from bsddb3 import db
 import os
 import sys
+import time
 from subprocess import call
 
-os.system('touch splitTerms.txt')
-os.system('touch splitAds.txt')
-os.system('touch splitPdates.txt')
-os.system('touch splitPrices.txt')
+def phase2():
 
-splitTerms = open('splitTerms.txt', 'w')
-splitAds = open('splitAds.txt', 'w')
-splitPdates = open('splitPdates.txt', 'w')
-splitPrices = open('splitPrices.txt', 'w')
+    os.system('touch splitTerms.txt')
+    os.system('touch splitAds.txt')
+    os.system('touch splitPdates.txt')
+    os.system('touch splitPrices.txt')
 
-termsFile = open("sortedTerms.txt")
-for line in termsFile:
-    line.rstrip()
-    #remove backslashes
-    line.replace("\\", "")
-    pair = line.split(":")
-    splitTerms.write(pair[0]+"\n")
-    splitTerms.write(pair[1])
-termsFile.close()
+    splitTerms = open('splitTerms.txt', 'w')
+    splitAds = open('splitAds.txt', 'w')
+    splitPdates = open('splitPdates.txt', 'w')
+    splitPrices = open('splitPrices.txt', 'w')
 
-adsFile = open("sortedAds.txt")
-for line in adsFile:
+    try:
+        termsFile = open('sortedTerms.txt')
+    except:
+        print("File could not be found\nExiting...")
+        time.sleep(1)
+        os.system(cls)
+        sys.exit()
+    for line in termsFile:
+        line.rstrip()
+        #remove backslashes
+        line.replace("\\", "")
+        pair = line.split(":")
+        splitTerms.write(pair[0]+"\n")
+        splitTerms.write(pair[1])
+    termsFile.close()
+
+    try:
+        adsFile = open('sortedAds.txt')
+    except:
+        print("File could not be found\nExiting...")
+        time.sleep(1)
+        os.system(cls)
+        sys.exit()
+    for line in adsFile:
         line.rstrip()
         #remove backslashes
         line.replace("\\", "")
         pair = line.split(":")
         splitAds.write(pair[0]+"\n")
         splitAds.write(pair[1])
-adsFile.close()
+    adsFile.close()
+
+    try:
+        pDatesFile = open('sortedPdates.txt')
+    except:
+        print("File could not be found\nExiting...")
+        time.sleep(1)
+        os.system(cls)
+        sys.exit()
+    for line in pDatesFile:
+        line.rstrip()
+        #remove backslashes
+        line.replace("\\", "")
+        pair = line.split(":")
+        splitPdates.write(pair[0]+"\n")
+        splitPdates.write(pair[1])
+    pDatesFile.close()
+
+    try:
+        pricesFile = open('sortedPrices.txt')
+    except:
+        print("File could not be found\nExiting...")
+        time.sleep(1)
+        os.system(cls)
+        sys.exit()
+    for line in pricesFile:
+        line.rstrip()
+        #remove backslashes
+        line.replace("\\", "")
+        pair = line.split(":")
+        splitPrices.write(pair[0]+"\n")
+        splitPrices.write(pair[1])
+    pricesFile.close()
+
+()
--- Sandbox/proj2.py
+++ Sandbox/proj2.py
@@ -34,10 +34,44 @@ def main():
     print("Sorted ads.txt")
     os.system("sort -u prices.txt > sortedPrices.txt")
     print("Sorted prices.txt")
-    os.system("sort -u pdate.txt > sortedPdate.txt")
+    os.system("sort -u pdate.txt > sortedPdates.txt")
     print("Sorted pdate.txt")
     os.system("sort -u terms.txt > sortedTerms.txt")
     print("Sorted terms.txt")
-    
 
+    phase2.phase2()
+
+    os.system("db_load -T -f splitAds.txt -t hash ad.idx")
+    print("Created ad.idx")
+    os.system("db_load -T -f splitPdates.txt -t btree da.idx")
+    print("Created da.idx")
+    os.system("db_load -T -f splitPrices.txt -t btree pr.idx")
+    print("Created pr.idx")
+    os.system("db_load -T -f splitTerms.txt -t btree te.idx")
+    print("Created te.idx")
+
+    cont = ""
+    while(cont.upper() != 'Y' and cont.upper() != 'N'):
+        cont = input("Would you like to continue to Phase 3? (Y/N): ")
+    if(cont == 'N'):
+        print("Phase 2 completed\nExiting...")
+        time.sleep(1)
+        os.system(cls)
+        sys.exit()
+
+    clean_up = ""
+    while(clean_up.upper() != 'Y' and clean_up.upper() != 'N'):
+        clean_up = input("Would you like to cclean up system files? (Y/N): ")
+    if(clean_up == 'Y'):
+        print("Cleaning up system files..."+
+              "Removing splitTerms.txt"+
+              "Removing splitPdates.txt"+
+              "Removing splitAds.txt"+
+              "Removing splitPrices.txt")
+        os.system("rm -rf splitTerms.txt splitPdates.txt splitAds.txt splitPrices.txt")
+        time.sleep(1)
+        os.system(cls)
+        sys.exit()
+
+    print("Entering Phase 3")
 main()
